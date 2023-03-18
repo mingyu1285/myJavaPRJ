4ae0cbe7e828290abe57af17f4dc63443ea19973
mingyu1285 <mingyu1285@naver.com>
2023-03-18 20:11:54 +0900

    Initial commit

---
+++ src/main/java/Mystudy.java
@@ -0,0 +1,19 @@
+public class Mystudy {
+    public static void main(String[] args) {
+
+
+            for ( int i=1; i<=5; i++){
+                for (int k=1; k<=i; k++){
+                    System.out.printf("*");
+                }
+                System.out.println("");
+            }
+        for ( int j=1; j<=5; j++){
+            for (int s=4; s>=j; s--){
+                System.out.printf("*");
+            }
+            System.out.println("");
+        }
+        }
+    }
+
