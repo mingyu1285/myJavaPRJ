64a1433f1b7b640451e3691100dba90d5d0432c2
mingyu1285 <56614072+mingyu1285@users.noreply.github.com>
2023-03-23 17:10:29 +0900

    Add files via upload

---
+++ Ex/Ex05_07.java
@@ -0,0 +1,43 @@
+public class Ex05_07 {
+    public static void main(String[] args) {
+        /* 중첩 if문
+        int a =75;
+
+        if (a>50){
+            if (a<100){
+                System.out.printf("50보다 크고 100보다 작군요..\n");
+            }else {
+                System.out.printf("100보다 크군요!\n");
+            }
+        }else{
+            System.out.printf("50보다 작네요");
+        }
+    }
+
+        / 중첩 if문 예제2
+
+        Scanner s = new Scanner(System.in);
+
+        int a;
+
+        System.out.printf("점수를 입력하세요 : ");
+        a = s.nextInt();
+
+        if (a>=90){
+            System.out.printf("A 학점");
+        } else if (a>=80) {
+            System.out.printf("B 학점");
+
+        }else if (a>=70){
+            System.out.printf("C 학점");
+        } else if (a>=60) {
+            System.out.printf("D 학점");
+
+        }else{
+            System.out.printf("F 학점");
+        }
+        System.out.printf("입니다.");
+        */
+    }
+}
+
---
+++ Ex/Ex05_09.java
@@ -0,0 +1,27 @@
+public class Ex05_09 {
+    public static void main(String[] args) {
+        /* Switch 문 연습
+        Scanner s =  new Scanner(System.in);
+        int a;
+
+        System.out.printf("본인의 점수가 몇점 이상인지 고르시오 <1> 90 <2> 80 <3> 70점 <4> 60점\n");
+        a= s.nextInt();
+
+        switch (a){
+            case 1:
+                System.out.printf("A 학점입니다.");
+                break;
+            case 2:
+                System.out.printf("B 학점입니다.");
+                break;
+            case 3:
+                System.out.printf("C 학점입니다.");
+                break;
+            case 4:
+                System.out.printf("D 학점입니다.");
+                break;
+        }
+
+         */
+    }
+}
