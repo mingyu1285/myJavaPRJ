ae5fdd26fe8c9951b3daa4b369e42ae93b8fd0a2
mingyu1285 <56614072+mingyu1285@users.noreply.github.com>
2023-04-26 23:52:04 +0900

    Add files via upload

---
+++ MyJavaStudy/Parameter2.java
@@ -0,0 +1,14 @@
+package MyStudy;
+
+public class Parameter2 {
+
+    public void speedUp(int value, int maxValue){
+        int speed = value;
+        int maxSpeed = maxValue;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
