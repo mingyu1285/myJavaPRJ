2d2e9304a1ceefa837ad85021b62d295108113a6
mingyu1285 <56614072+mingyu1285@users.noreply.github.com>
2023-04-27 23:50:31 +0900

    Add files via upload

---
+++ MyJavaStudy/MethodOverLoding.java
@@ -0,0 +1,11 @@
+package MyStudy;
+
+public class MethodOverLoding {
+    public void speedUp(int value){
+        int speed = value;
+    }
+    public void speedUp(int value, int maxValue){
+        int speed = value;
+        int maxSpeed = maxValue;
+    }
+}
