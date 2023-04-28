ac7adcd8d6f6043071d9541d487fd460e06fbc94
mingyu1285 <56614072+mingyu1285@users.noreply.github.com>
2023-04-28 21:55:19 +0900

    Add files via upload

---
+++ Quiz/Quiz4.java
@@ -0,0 +1,13 @@
+package Quiz;
+
+public class Quiz4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 2; j <=9; j++) {
+                System.out.printf("%d X %d = %d \t", j,i,j*i);
+            }
+            System.out.println();
+
+        }
+    }
+}
